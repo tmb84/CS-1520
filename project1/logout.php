1cacae1465a0fa80172813bb9f9456ed371c647e
tmb84 <taylor7brown@gmail.com>
2016-04-30 14:36:47 -0400

    all files

--- project1/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if(session_destroy()) // Destroying All Sessions
-{
-header("Location: home.php"); // Redirecting To Home Page
-}
-?>
--- project1/menu.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '<a href="home.php">Home</a> *
-<a href="admin.php">Post</a> *
-<a href = "index2.php">Login</a> *
-<a href = "logout2.php">Logout</a>*
-<a href = "signup.php">Signup</a>*
-<a href="contactForm.php">Contact</a> ';
-
-?>
